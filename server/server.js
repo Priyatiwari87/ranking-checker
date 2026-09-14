f05b61293f1ab89ba28cc4f809954e970ba16ab4
Priyatiwari87 <priyabth8797@gmail.com>
2026-09-15 00:47:02 +0530

    Fix Express 5 SPA route matching for Render

--- server/server.js
+++ server/server.js
@@ -60,8 +60,11 @@ app.use('/api', apiRoutes);
 const path = require('path');
 if (process.env.NODE_ENV === 'production' || process.env.RENDER) {
   app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
+  app.use((req, res, next) => {
+    if (req.method === 'GET' && !req.path.startsWith('/api')) {
+      return res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
+    }
+    next();
   });
 }
 
